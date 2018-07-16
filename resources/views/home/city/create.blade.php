d71df97391498ffe3658dd6e044b12513085c468
Mohamed Eid <meid368@gmail.com>
2018-07-16 09:29:49 +0200

    Update create.blade.php

--- resources/views/home/city/create.blade.php
+++ resources/views/home/city/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-	<form action="{{ url('city') }}" method="post" class="form-group">
+	<form action="{{ url('admin/city') }}" method="post" class="form-group">
 		@csrf
 		<label for="">Name</label>
 		<input type="text" name="name" class="form-control">
